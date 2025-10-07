47fa45cb45373df6de397fcefd683649c557feaf
Harshal <thakareharshal73@gmailcom>
2025-10-07 20:52:21 +0530

    Variables and datatypes

---
+++ basic_01/01_variables.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 20;
+console.log(a+b); 
+
+const accountId = 12214;
+let accountEmail = "harshal@gmail.com";
+var accountPassword = "12231";
+accountCity = "Pune";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+ 
