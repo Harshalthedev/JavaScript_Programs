6049e590ad072ec9977ba5e9d2be5c42333cfc97
Harshal <thakareharshal73@gmailcom>
2025-10-08 12:28:18 +0530

    Comparisions

--- basic_01/02_dataTypes.js
+++ basic_01/02_dataTypes.js
@@ -13,3 +13,44 @@
 
 console.log(typeof(null));            //null is a type of object
 console.log(typeof(undefined));       // undefined is a type of undefined
+
+
+//primitive
+
+//7 types  String, boolean, null, undefined, Number, Symbol, BigInt
+
+
+
+// is js a static or dynamic language --> dynamic langauge
+
+
+//here symbol is used to identity something unique, theirfore eventhough they have the same value it is diffrent
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId) // false
+
+
+// BigInt -> use n at the end of the number
+
+const bigInt = 123456789456123355555555n;
+console.log(bigInt);
+
+//Reference (Non Primitive)
+
+// Arrays, Objects, Function
+
+
+const myArray = ["jay", "sham", "raj", "ram"];
+let myObject = {
+    name : "harshal",
+    age : 21,
+    city : "pune"
+}
+
+const myFunction = function() {
+    console.log("hello");
+}
+
+console.log(typeof myArray)
+console.log(typeof myObject)
+console.log(typeof myFunction)
--- basic_01/03_conversionOperations.js
+++ basic_01/03_conversionOperations.js
@@ -25,3 +25,29 @@ let num = 265;
 let num1 = String(num);
 console.log(typeof(num1))
 console.log(num1)
+
+
+//operations
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  //here it is 2 power 2
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello";
+let str2 = " harshal";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);  // 12
+console.log(1 + "2");  // 12
+console.log("1" + 2 + 2);  // 122
+console.log(1 + 2 + "2");  // 32
